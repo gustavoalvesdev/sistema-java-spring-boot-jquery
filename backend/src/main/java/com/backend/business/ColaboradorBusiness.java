436c7eb3b0664b0a08d1cd911aff390a9935bab2
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-12-05 18:07:26 -0300

    Criei o método de calcular a complexidade da senha na classe ColaboradorBusiness

--- backend/src/main/java/com/backend/business/ColaboradorBusiness.java
+++ backend/src/main/java/com/backend/business/ColaboradorBusiness.java
@@ -23,6 +23,24 @@ public class ColaboradorBusiness {
 	}
 	
 	public ColaboradorEntity save(ColaboradorEntity colaboradorEntity) {
+		colaboradorEntity.setScore(calcularComplexidade(colaboradorEntity.getSenha()));
+		
 		return colaboradorRepository.save(colaboradorEntity);
+	}
+	
+	public int calcularComplexidade(String password) {
+		int lengthScore = Math.min(password.length() / 2, 5);
+        int uppercaseScore = password.matches(".*[A-Z].*") ? 5 : 0;
+        int lowercaseScore = password.matches(".*[a-z].*") ? 5 : 0;
+        int digitScore = password.matches(".*\\d.*") ? 5 : 0;
+
+        // You can customize the special characters based on your requirements
+        String specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/";
+        int specialCharScore = password.matches(".*[" + specialChars + "].*") ? 5 : 0;
+
+        // You can adjust weights for different criteria based on your requirements
+        int totalScore = lengthScore + uppercaseScore + lowercaseScore + digitScore + specialCharScore;
+
+        return totalScore;
 	}
 }
